2baca891811d3136528e6bfb8361c9e0624a0e95
Stefano Salvatori <ssalvatori@gmail.com>
2014-01-24 10:47:17 -0300

    added onclick event in listview

---
+++ src/com/ssalvatori/fondosmutuos/DetailsActivity.java
@@ -0,0 +1,15 @@
+package com.ssalvatori.fondosmutuos;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class DetailsActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_details);
+		// Show the Up button in the action bar.
+	}
+
+}
